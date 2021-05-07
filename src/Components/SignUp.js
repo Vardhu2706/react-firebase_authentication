cce17adfe5470eb0965441dbff02f814919e5421
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-05-07 14:37:08 +0530

    Created Auth Context, added Sign-Up functionality, set Auth Loading state.

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,22 +1,47 @@
 // Importing Helpers
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 // Importing Components/ Files
-import { Form, Button, Card } from "react-bootstrap";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../Contexts/AuthContext";
 
 const SignUp = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signup, currentUser } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match.");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError("Failed to create an account.");
+    }
+
+    setLoading(false);
+  }
 
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center">Sign Up</h2>
+          
+          {/* Error Alert */}
+          {error && <Alert variant="danger">{error}</Alert>}
 
           {/* Sign Up Form */}
-          <Form>
+          <Form onSubmit={handleSubmit}>
             {/* Email Field Form Group */}
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
@@ -43,7 +68,7 @@ const SignUp = () => {
               ></Form.Control>
             </Form.Group>
 
-            <Button className="w-100 mt-4" type="submit">
+            <Button className="w-100 mt-4" type="submit" disabled={loading}>
               Sign Up
             </Button>
           </Form>
