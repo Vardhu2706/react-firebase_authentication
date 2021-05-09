50a64b8d2e9d0cc1633f9e22d58453fb0b71d973
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-05-09 22:33:00 +0530

    Added Dashboard Component functionality.

--- src/Components/DashBoard.js
+++ src/Components/DashBoard.js
@@ -1,11 +1,36 @@
 // Importing Helpers
-import React from "react";
+import React, { useState } from "react";
+import { Card, Button, Alert } from "react-bootstrap";
+import { useAuth } from "../Contexts/AuthContext";
+import { Link } from "react-router-dom";
 
 // Importing Components/ Files
 
 // Functional Component
 const DashBoard = () => {
-  return <div>DashBoard</div>;
+  const [error, setError] = useState();
+  const { currentUser } = useAuth();
+
+  function handleLogout() {}
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email:</strong> {currentUser.email}
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+            Update Profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>
+          Logout
+        </Button>
+      </div>
+    </>
+  );
 };
 
 // Default Export
